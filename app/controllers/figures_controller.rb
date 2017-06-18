11c10319016b4db140cae74dc7c2529b8fca49c0
Villy Siu <villysiu@gmail.com>
2017-06-18 23:49:01 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,58 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do
+
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    puts params["figure"]
+    @figure = Figure.create(params["figure"])
+
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(params["title"])
+    end
+    
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(params["landmark"])
+    end
+    @figure.save
+    redirect "figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(params["title"])
+    end
+    
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(params["landmark"])
+    end
+      @figure.save
+
+    redirect "figures/#{@figure.id}"
+  end
+end
---
+++ db/migrate/20170616220458_create_figure.rb
@@ -0,0 +1,7 @@
+class CreateFigure < ActiveRecord::Migration
+  def change
+    create_table :figures do |t|
+      t.string :name
+    end
+  end
+end
